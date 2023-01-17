a08eaf39d6094c880ba7ee547b57122003873d03
Jezreel Villaruel <122867032+FPSamurai30@users.noreply.github.com>
2023-01-17 19:02:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,10 +44,61 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
     var username = document.getElementById("signup-username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("signup-password").value;
-    console.log("Username: " + username + " Email: " + email + " Password: " + password);
+  
+    // validate the data
+    if (!username || !email || !password) {
+      alert("All fields are required.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      alert("Invalid email address.");
+      return;
+    }
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      return;
+    }
+  
+    // encrypt the password
+    var encryptedPassword = encryptPassword(password);
+  
+    // create an object with the user data
+    var userData = {
+      username: username,
+      email: email,
+      password: encryptedPassword
+    };
+  
+    // send the user data to the server
+    fetch("https://github.com/finalexam_1st_sem_ads1wst", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      // check the response from the server
+      if (data.success) {
+        alert("Sign up successful! Please log in.");
+        switchForm();
+      } else {
+        alert("Error: " + data.message);
+      }
+    })
+    .catch(error => console.log(error));
+  }
+  
+  function validateEmail(email) {
+    // check if the email is valid
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
   
     // Send the sign up data to the server or save it in the database
     // Here you can add validation and encryption of the data
-  }
+  
   
   
